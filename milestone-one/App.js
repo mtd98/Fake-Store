d8e905a55b8198838b701dca9d1aec72814246c5
Kaaya <masontdunbar@live.com>
2024-04-23 20:19:38 +1000

    Milestone 1 - Added Styling and images to splashscreen

--- milestone-one/App.js
+++ milestone-one/App.js
@@ -16,7 +16,7 @@ export default function App() {
   useEffect(() => {
     setTimeout(() => {
       setAppIsReady(true);
-    }, 3000);
+    }, 6000);
   }, []);
 
   if (!appIsReady) {
--- milestone-one/src/screens/SplashScreen.js
+++ milestone-one/src/screens/SplashScreen.js
@@ -1,15 +1,46 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, ImageBackground, Image } from 'react-native';
 
 export default function SplashScreen({ navigation }) {
   return (
-    <View>
-      <Text>Splash Screen</Text>
+    <ImageBackground source={require('../img/Background.jpeg')} style={styles.background}>
+    <View style={styles.container}>
+      <View style={styles.logoContainer}>
+        <Text style={styles.logoText}>Fake Store</Text>
+        <Image source={require('../img/ShoppingBag.png')} style={styles.shoppingBags} />
+      </View>
     </View>
+  </ImageBackground>
   );
 }
 
 const styles = StyleSheet.create({
-
+  background: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  container: {
+    alignItems: 'center',
+  },
+  logoContainer: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    flexDirection: 'row',
+  },
+  logoText: {
+    fontSize: 40,
+    fontWeight: 'bold',
+    color: 'white',
+    textShadowColor: 'rgba(0, 0, 0, 0.75)',
+    textShadowOffset: { width: -1, height: 1 },
+    textShadowRadius: 10,
+  },
+  shoppingBags: {
+    width: '30%', 
+    height: undefined, 
+    aspectRatio: 1,
+    resizeMode: 'contain',
+  },
 });
 
