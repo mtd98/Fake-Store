1a8e8f372754e6c584945e04624584d317870b7c
Kaaya <masontdunbar@live.com>
2024-05-09 12:33:40 +1000

    Milestone 2 - Fixed Navigation Bug

--- milestone-two/src/components/Icon.js
+++ milestone-two/src/components/Icon.js
@@ -3,10 +3,8 @@ import { Ionicons } from "@expo/vector-icons";
 
 export const Icon =({ name, fun}) => {
   return (
-    <Pressable onPress={fun}>
       <View>
         <Ionicons name={name} color="black" size={30}/>
       </View>
-    </Pressable>
   );
 };
